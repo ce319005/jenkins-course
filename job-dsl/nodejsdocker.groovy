02bb731d14cb58356c374900110785facab851f8
Jeremia Agung Panjaitan <65180313+ce319005@users.noreply.github.com>
2022-04-20 17:24:33 +0700

    Update nodejsdocker.groovy

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -14,7 +14,7 @@ job('NodeJS Docker example') {
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('ce319005/docker-nodejs-demo')
+            repositoryName('wardviaene/docker-nodejs-demo')
             tag('${GIT_REVISION,length=9}')
             registryCredentials('dockerhub')
             forcePull(false)
